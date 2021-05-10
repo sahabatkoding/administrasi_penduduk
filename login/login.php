9cf73e3f4f9cc238ec86a2a42b00392c5cb94a7a
ajieijie <triaji20@gmail.com>
2021-05-10 12:37:38 +0700

    Update login.php

--- login/login.php
+++ login/login.php
@@ -5,11 +5,14 @@ require_once '../assets/library/anti_inject.php';
 $username = anti_inject($_POST['username']);
 $password = anti_inject(md5($_POST['password']));
 
-$sql = "SELECT * FROM ap_user WHERE username = '$username' AND password = '$username' ";
-$result = $koneksi->query($qsl);
+$sql = "SELECT * FROM ap_user WHERE username = '$username' AND password = '$password' ";
+$result = $koneksi->query($sql);
 $rows = $result->num_rows;
 $data = $result->fetch_array();
 
-var_dump($sql);
+var_dump($rows);
 
+iF($rows>0){
+echo 'asd';
+}
  ?>
