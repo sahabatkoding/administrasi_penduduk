5f9de811f78395bf422d38852044c0a23104ed16
ajieijie <triaji20@gmail.com>
2021-05-21 18:45:14 +0700

    Create function.php

---
+++ master/assets/library/function.php
@@ -0,0 +1,11 @@
+
+<script>
+// hanya angka
+function hanyaAngka(evt) {
+    var charCode = (evt.which) ? evt.which : event.keyCode
+     if (charCode > 31 && (charCode < 48 || charCode > 57))
+
+      return false;
+    return true;
+  }
+</script>
