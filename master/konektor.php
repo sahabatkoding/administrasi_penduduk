ed5169d0412b26da7ad6d23b26c0075714115a92
ajieijie <triaji20@gmail.com>
2021-06-24 18:31:51 +0700

    dq

--- master/konektor.php
+++ master/konektor.php
@@ -1,4 +1,5 @@
 <?php 
+error_reporting(0);
 require_once '../../database/config.php';
 $ROOT = '../';
 $MASTER = '../../';
---
+++ master/kota/index.php
@@ -0,0 +1,219 @@
+<?php 
+require_once '../konektor.php';
+require_once $LIB.'session.php';
+
+if($admin==0){
+  ?>
+  <script>location.href="<?=$MASTER?>login/logout.php"</script>
+  <?php
+}
+
+
+$header = "Data Provinsi";
+
+ ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<?php require_once $LAYOUT.'head.php'; ?>
+</head>
+<body>
+	<?php require_once $LAYOUT.'header.php'; ?>
+	<?php require_once $LAYOUT.'sidebar.php'; ?>
+
+	<div class="mobile-menu-overlay"></div>
+
+	<div class="main-container">
+		<div class="pd-ltr-20 xs-pd-20-10">
+			<div class="min-height-200px">
+				<!-- <div class="page-header">
+					<div class="row">
+						<div class="col-md-6 col-sm-12">
+							<div class="title">
+								<h4>DataTable</h4>
+							</div>
+							<nav aria-label="breadcrumb" role="navigation">
+								<ol class="breadcrumb">
+									<li class="breadcrumb-item"><a href="index.html">Home</a></li>
+									<li class="breadcrumb-item active" aria-current="page">DataTable</li>
+								</ol>
+							</nav>
+						</div>
+						<div class="col-md-6 col-sm-12 text-right">
+							<div class="dropdown">
+								<a class="btn btn-primary dropdown-toggle" href="#" role="button" data-toggle="dropdown">
+									January 2018
+								</a>
+								<div class="dropdown-menu dropdown-menu-right">
+									<a class="dropdown-item" href="#">Export List</a>
+									<a class="dropdown-item" href="#">Policies</a>
+									<a class="dropdown-item" href="#">View Assets</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div> -->
+				<!-- Simple Datatable start -->
+				<div class="card-box mb-30">
+					<div class="pd-20">
+						<h4 class="text-blue h4"><?= $header ?>
+
+						<span class="pull-right">
+							<button class="btn btn-primary " data-toggle="modal" data-target="#modal" id="tambah" name="tambah">Tambah</button>
+						</span>
+						</h4>
+					</div>
+					<div class="pb-20">
+						<!-- <table class="table striped hover nowrap" id="table" width="100%"> -->
+							<table class="table hover stripe nowrap table-bordered" id="table" width="100%">
+							<thead>
+								<tr>
+									<th>No</th>
+									<th>Provinsi</th>
+									<th>Detail</th>
+									<th>Edit</th>
+									<th>Hapus</th>
+								</tr>
+							</thead>
+						</table>
+					</div>
+				</div>
+				<!-- Simple Datatable End -->
+				<!-- modal -->
+
+                      <div class="modal fade" id="modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog" role="document">
+                          <div class="modal-content">
+                            <div class="modal-header">
+                              <h5 class="modal-title" id="exampleModalLabel"><?php echo $Header ?></h5>
+                              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                              </button>
+                            </div>
+                            <form id="modal_form">
+                              <input type="text" name="pendidikan_id" id="pendidikan_id">
+                              <div class="modal-body">
+                                <label for="">Pendidikan</label>
+                                <input type="text" name="pendidikan_nama" id="pendidikan_nama" class="form-control">
+                              </div>
+                              <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                <button type="button" class="btn btn-primary" id="simpan">Simpan</button>
+                                <button type="button" class="btn btn-success" id="edit" style="display:none">Edit</button>
+                              </div>
+                            </form>
+                          </div>
+                        </div>
+                      </div>
+                      <!-- modal -->
+			</div>
+			<?php require_once $LAYOUT.'footer.php'; ?>
+		</div>
+	</div>
+	<!-- js -->
+	<script>
+	 	function kosong(){
+           $('#pendidikan_id').val('');
+           $('#modal_form')[0].reset();
+           $('#edit').css('display','none');
+           $('#simpan').css('display','inline-block');
+         }
+
+
+	$(function () {
+           /* Isi Table */
+             $('#table').DataTable({
+             	   dom: "Bfrtip",
+       			 buttons: [
+            'copy', 'csv', 'excel', 'pdf', 'print'
+       					 ],
+               "scrollX": true,
+             
+               "ajax": {
+                   "url": "data.php",
+                   "dataSrc": ""
+                 },
+                 "columns": [
+                   {"data": "no"},
+                   {"data": "pendidikan"},
+                   {"data": "edit"},
+                   {"data": "hapus"},
+                 ]
+             });
+           /* Isi Table */
+         });
+
+
+         $('#tambah').on('click',function(){
+         	kosong();
+         })
+
+         $('#simpan').on('click',function(){
+         	$.ajax({
+         		url: 'proses.php',
+         		type: 'POST',
+         		dataType: 'HTML',
+         		data: $('#modal_form').serialize(),
+         		// console.log(data);
+         		success:function(isi){
+         		$('#modal').modal('hide');
+                kosong();
+                $('#table').DataTable().ajax.reload();
+         		}
+         	})
+         	.fail(function() {
+         		console.log("error");
+         	})         	
+         })
+
+         function edit(isi){
+         	$('#pendidikan_id').val(isi);
+         	$('#edit').css('display','inline-block');
+         	$('#simpan').css('display','none');
+         	$.getJSON('data.php', {id: isi}, function(json) {
+         			$('#pendidikan_nama').val(json.pendidikan_nama);
+         	});
+         }
+
+         $('#edit').on('click',function(){
+         	$.ajax({
+         		url:'proses.php',
+         		type:'POST',
+         		dataType:'HTML',
+         		data:$('#modal_form').serialize(),
+         		success:function(isi){
+         			$('#modal').modal('hide');
+         			kosong();
+         			$('#table').DataTable().ajax.reload()
+         		}
+         	})
+         	.fail(function(){
+         		console.log("error");
+         	})
+         })
+
+         function hapus(isi){
+         	var n = confirm('Yakin Hapus?');
+
+         	if(n){
+         	$.ajax({
+         		url:'proses.php',
+         		type:'GET',
+         		dataType:'HTML',
+         		data: {id:isi},
+         		success:function(isi){
+         			$('#modal').modal('hide');
+         			kosong();
+         			$('#table').DataTable().ajax.reload()
+         		}
+         	})
+         	.fail(function(){
+         		console.log("error");
+         	})	
+         	}
+         }
+	</script>
+	<?php require_once $LAYOUT.'js.php'; ?>
+</body>
+</html>
